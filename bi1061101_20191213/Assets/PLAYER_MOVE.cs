5f47a0bc28159b039579fc7a6bb5106df1d582e8
ojy999 <michelleojy@gmail.com>
2019-12-13 12:11:48 +0800

    character&scene

---
+++ bi1061101_20191213/Assets/PLAYER_MOVE.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLAYER_MOVE : MonoBehaviour {
+#region parameters
+
+    [Header("Moving speed"), Range(0,2000)]
+    public float speed = 1.5f;
+    [Header("HP amount"), Range(0, 1000)]
+    public float hp = 1.5f;
+
+    public Animator anim;
+    public RigidBody2D r2D;
+
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+        r2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
