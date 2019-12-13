216096fe1500598b2f2d10c073922b27ccd8c4bc
ojy999 <michelleojy@gmail.com>
2019-12-13 15:05:12 +0800

    BulletsShot

--- bi1061101_20191213/Assets/PLAYER_MOVE.cs
+++ bi1061101_20191213/Assets/PLAYER_MOVE.cs
@@ -7,22 +7,66 @@ public class PLAYER_MOVE : MonoBehaviour {
 
     [Header("Moving speed"), Range(0,2000)]
     public float speed = 1.5f;
-    [Header("HP amount"), Range(0, 1000)]
-    public float hp = 1.5f;
 
-    public Animator anim;
-    public RigidBody2D r2D;
+    [Header("HP amount"), Range(100, 1000)]
+    public float hp = 100;
 
+    [Header("Bullet")]
+    public GameObject bullet;
+
+    [Header("ShootAudio")]
+    public AudioClip shootAUD;
+
+    [Header("Instantiate Position")]
+    public Transform point;
+
+    public float BulletSpeed = 1000f;
+
+    private Animator anim;
+    private Rigidbody2D r2D;
+    private AudioSource sound;
+
+    
+    
     #endregion
 
     // Use this for initialization
-    void Start () {
+    private void Start () {
         r2D = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-	}
+        sound = GetComponent<AudioSource>();
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
+	private void FixedUpdate () {
+		Move();
 	}
+
+    private void Update()
+    {
+        Fire();
+    }
+
+    private void Move(){
+
+        float controlX = Input.GetAxisRaw("Horizontal");
+        //float controly = Input.GetAxisRaw("Vertical");
+
+         r2D.AddForce(new Vector2(speed*controlX,0));
+        // r2D.AddForce(new Vector2(0,speed*controly));
+
+        if(controlX != 0){ transform.localScale = new Vector3(controlX,1,1);}
+
+    }
+
+    private void Fire() {
+
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+            sound.PlayOneShot(shootAUD);
+            GameObject pewpewpew = Instantiate(bullet,point.position, Quaternion.identity);
+            pewpewpew.GetComponent<Rigidbody2D>().AddForce(new Vector2(BulletSpeed*transform.localScale.x,0));
+
+        }
+
+    }
 }
